4b93bfd5b9184586d8147b31ddaa03196ebfe1ad
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-11-15 18:20:17 -0800

    Addd pagination to the players page. First and Last links are not showing up for some reason.

--- application/controllers/Players.php
+++ application/controllers/Players.php
@@ -14,6 +14,9 @@ class Players extends Application {
     
     function __construct() {
         parent::__construct();
+        $this->load->library('pagination');
+        $this->load->helper('url');
+        //$this->load->library('uri');
     }
 
     /**
@@ -24,15 +27,38 @@ class Players extends Application {
         // this is the view we want shown
         $this->data['pagebody'] = 'players_view';
         
+        //Pagination stuff
+        $config['base_url'] = base_url().'players/index';
+        $config['total_rows'] = $this->players_list->record_count();
+        $config['per_page'] = 12; 
+        $config['first_url'] = '1';
+        $config['last_url'] = '1';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['first_link'] = 'First';
+        $config['last_link'] = 'Last';
+
+        $this->pagination->initialize($config);
+
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+        $source = 
+                $this->players_list->get($config["per_page"], $page);
+
+        $this->data["links"] = $this->pagination->create_links();
+
+
         // build the list of players, to pass on to our view
-        $source = $this->players_list->all();
-        
         $players = array();
         foreach ($source as $record) {
             $players[] = array('firstname' => $record['FIRSTNAME'], 'lastname' => $record['LASTNAME'], 'teamcode' => $record['TEAMCODE'],
                 'playernum' => $record['PLAYERNUM'], 'position' => $record['POSITION'], 'info' => $record['INFO'], 'image' => $record['IMAGE']);
         }
         $this->data['players'] = $players;
+
+        $config['base_url'] = base_url();
+
+
 
         $this->render();
     }
