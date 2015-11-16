2b88ad67d7a611f9e77d867cc20b2dc67a9bb63b
Becky <beckyzhou@outlook.com>
2015-11-16 01:50:29 -0800

    Aesthetic changes

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,4 +1,9 @@
-<div >
+<div>
+    
+    <p id="welcome_text">
+        The Miami Dolphins are a professional American football team located in the Miami metropolitan area. They play their home games at Sun Life Stadium in the northern suburb of Miami Gardens, and have their headquarters in Davie, Florida.
+    </p>
+    
     <img id="stadium" src="assets/images/stadium.jpg">
     
     <table id="membertable">
