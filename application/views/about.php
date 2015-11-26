d9add279b3a66f571230b4283888d425f99a51dd
Becky <beckyzhou@outlook.com>
2015-11-26 15:28:36 -0800

    Fixed typos on about page

--- application/views/about.php
+++ application/views/about.php
@@ -2,9 +2,9 @@
     <img id="img_players" src="assets/images/players.jpg">
     
     <div id="about_text">
-        <p>This sie was designed and created by Team Miami Dolphins for COMP4711.</p>
+        <p>This site was designed and created by Team Miami Dolphins for COMP4711.</p>
             
-        <p>This site is made purely for academic learning.</p>
+        <p>Everything was done purely for academic learning.</p>
 
         <p>All uses of names and media fall under the fair use guidelines.</p>
     </div>
