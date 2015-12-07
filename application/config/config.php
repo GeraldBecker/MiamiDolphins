33aa33b04985a577c4c6bf90637c4425a88a4f1e
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-12-06 18:12:20 -0800

    Made all predictions. Miami will lose.

--- application/config/config.php
+++ application/config/config.php
@@ -6,7 +6,7 @@ $config['menu_choices'] = array(
     	array('name' => 'Home', 'link' => '/'),
         array('name' => 'Roster', 'link' => '/players'),
         array('name' => 'Teams', 'link' => '/teams'),
-        array('name' => 'Download Scores', 'link' => '/ScoreDownload'),
+        array('name' => 'Download Scores', 'link' => '/Scoredownload'),
         array('name' => 'About', 'link' => '/about')
         )
     );
